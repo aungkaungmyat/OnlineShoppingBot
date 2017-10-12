e344b8a521903154c50e401de16ccb22d4e09659
aungkaungmyat <amyat@purdue.edu>
2017-10-12 12:52:10 -0400

    added token

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var express = require("express");
+/*var express = require("express");
 var request = require("request");
 var bodyParser = require("body-parser");
 
@@ -21,4 +21,32 @@ app.get("/webhook", function(req,res){
         console.error("Verification failed. The tokens do not match");
         res.sendStatus(403);
     }
-})
+})*/
+
+
+var express = require("express");
+var request = require("request");
+var bodyParser = require("body-parser");
+
+var app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.listen((process.env.PORT || 5000));
+
+// Server index page
+app.get("/", function (req, res) {
+  res.send("Deployed!");
+});
+
+// Facebook Webhook
+// Used for verification
+app.get("/webhook", function (req, res) {
+    console.log(req.query)
+  if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
+    console.log("Verified webhook");
+    res.status(200).send(req.query["hub.challenge"]);
+  } else {
+    console.error("Verification failed. The tokens do not match.");
+    res.sendStatus(403);
+  }
+});
